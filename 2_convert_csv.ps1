254f2728a21cede0b21fa938f95c59f39837d3f7
chen-yixin <chen-yixin@outlook.com>
2025-03-08 16:46:22 +0800

    目录结构调整

--- 2_convert_csv.ps1
+++ 2_convert_csv.ps1
@@ -1,6 +1,6 @@
 # 定义输入文件路径和输出CSV文件路径
-$inputFiles = Get-ChildItem -Path ".\logs" -Filter "fakessh*.log"
-$outputCsv = ".\outputs\fakessh_ps.csv"
+$inputFiles = Get-ChildItem -Path ".\upstream" -Filter "fakessh*.log"
+$outputCsv = ".\downstream\output_2.csv"
 
 # 初始化一个数组来存储结果
 $results = @()
