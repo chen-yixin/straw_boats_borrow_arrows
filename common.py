4c812092070a63949cfcb89f0d2f9c61c6f95003
chen-yixin <chen-yixin@outlook.com>
2025-03-12 21:56:47 +0800

    密码分析

--- common.py
+++ common.py
@@ -4,4 +4,5 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent
 UPSTREAM_DIR = BASE_DIR / "upstream"
 DOWNSTREAM_DIR = BASE_DIR / "downstream"
-
+OUTPUT_DIR = BASE_DIR / "output"
+DATASET_DIR = BASE_DIR / "dataset"
